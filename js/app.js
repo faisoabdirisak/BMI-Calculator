fd4417e11ecb2df3a38b1cb63a70ffce4f54336c
faisoabdirisak <faisoabdirisak@gmail.com>
2021-11-03 17:55:46 +0300

    a

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const height = document.querySelector("#height");
+const weight = document.querySelector("#weight");
+const calculate = document.querySelector("#calculate");
+const yourBMI = document.querySelector("#yourBMI");
+
+calculate.addEventListener("click" , ()=>{
+    if(height.value !="" && weight.value !=""){
+        let bmiValue = weight.value / (height.value*height.value) * 10000;
+        yourBMI.innerHTML= "Your IBM Is : " +bmiValue.toFixed(2);
+    }
+    else {
+        yourBMI.innerHTML = "please Enter correct value"
+    }
+});
